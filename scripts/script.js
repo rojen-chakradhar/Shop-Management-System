56501ec8f485872a934aff20d1685db4989f6ff6
Rojen Chakradhar <chakradhar@rojen.me>
2025-09-12 18:47:49 +0545

    sidenav completed?

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+// let list = document.querySelectorAll('.sidenav-item');
+// let listIcon = document.querySelectorAll('.sidenav-item .icon');
+// list.forEach((elm) =>
+//   elm.addEventListener('click', function () {
+//     list.forEach((item) => item.classList.remove('active'));
+//     this.classList.add('active');
+//     listIcon.forEach((icon) => icon.classList.remove('active-icon'));
+//     this.querySelector('.icon').classList.add('active-icon');
+//   })
+// );
